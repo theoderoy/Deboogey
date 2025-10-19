3fc59b2887578dc08034fea230f7fae7df555dc2
theoderoy <jzb4bhg7mr@privaterelay.appleid.com>
2025-10-19 14:18:14 +0200

    EducationPlayerView for utility views

---
+++ Deboogey/Views/EducationPlayerView.swift
@@ -0,0 +1,53 @@
+//
+//  EducationPlayerView.swift
+//  Deboogey
+//
+//  Created by Théo De Roy on 19/10/2025.
+//
+
+import SwiftUI
+import AVKit
+
+struct EducationPlayerView: NSViewRepresentable {
+    let name: String
+    let fileExtension: String
+
+    func makeNSView(context: Context) -> AVPlayerView {
+        let playerView = AVPlayerView()
+        playerView.wantsLayer = true
+        playerView.layer?.backgroundColor = NSColor.clear.cgColor
+        playerView.controlsStyle = .none
+        playerView.showsFullScreenToggleButton = false
+        playerView.videoGravity = .resizeAspectFill
+        playerView.translatesAutoresizingMaskIntoConstraints = false
+
+        if let url = Bundle.main.url(forResource: name, withExtension: fileExtension) {
+            let playerItem = AVPlayerItem(url: url)
+            let player = AVQueuePlayer(items: [playerItem])
+            player.isMuted = true
+            let looper = AVPlayerLooper(player: player, templateItem: playerItem)
+            context.coordinator.player = player
+            context.coordinator.looper = looper
+            playerView.player = player
+            player.play()
+        }
+
+        return playerView
+    }
+
+    func updateNSView(_ nsView: AVPlayerView, context: Context) {
+        nsView.player?.isMuted = true
+        if nsView.player?.rate == 0 {
+            nsView.player?.play()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator {
+        var player: AVQueuePlayer?
+        var looper: AVPlayerLooper?
+    }
+}
